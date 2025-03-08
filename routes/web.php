f6e0809518bcc32f48b1ee2a48718fd5dc9c4fbc
Matheus <matheusmartinscia@gmail.com>
2025-03-08 17:03:09 -0300

    MainController created and routes estabilished

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+//AUTH
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/authLogin', [AuthController::class, 'loginSubmit'])->name('authLogin');
+
+//MAIN
+Route::get('/', [MainController::class, 'home'])->name('home');
