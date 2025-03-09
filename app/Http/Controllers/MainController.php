cd57a971ee15049a7edd71717a975f55b2e9f7e7
Matheus <matheusmartinscia@gmail.com>
2025-03-09 17:55:25 -0300

    Update contact functionality has been created

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,12 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Services\MainService;
+use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function home() {
+    public function home()
+    {
         $contacts = MainService::getContacts(session('user.id'));
-
         return view('home', ['contacts' => $contacts]);
+    }
+
+    public function update($id)
+    {
+        $contact = MainService::getContact($id);
+        return view('update_contact', ['contact' => $contact]);
+    }
+
+    public function updateSubmit(Request $request)
+    {
+        filter_var($request['email'], FILTER_VALIDATE_EMAIL);
+
+        $request->validate(
+            [
+                "email" => ['required', 'email:rfc'],
+                "username" => ['required', 'min:6', 'max:16']
+            ],
+            [
+                "email.required" => "EMAIL OBRIGATÓRIO",
+                "email.email" => "EMAIL INVÁLIDO",
+                "username.required" => "SENHA OBRIGATÓRIA",
+                "username.min" => "O NÚMERO MINIMO DE CARACTERES É :min",
+                "username.max" => "O NÚMERO MÁXIMO DE CARACTERES É :max"
+            ]
+        );
+
+        $result = MainService::updateContact($request);
+
+        if($result['status']) {
+            return redirect()->route('home');
+        }
+
+        return redirect()
+            ->back()
+            ->withInput()
+            ->with('updateError', $result['msg']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ Route::post('/authLogin', [AuthController::class, 'loginSubmit'])->name('authLog
 //Logged
 Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/updateContact/{id}', [MainController::class, 'update'])->name('update');
+Route::post('/updateContactSubmit', [MainController::class, 'updateSubmit'])->name('updateSubmit');
