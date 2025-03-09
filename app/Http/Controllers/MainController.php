88461f43a7838ac641ff4f4cdb0fd0fa161b268e
Matheus <matheusmartinscia@gmail.com>
2025-03-09 15:07:15 -0300

    Create MainController and MainService

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MainService;
 
 class MainController extends Controller
 {
     public function home() {
+        $contacts = MainService::getContacts(session('user.id'));
 
-        return view('home');
+        return view('home', ['contacts' => $contacts]);
     }
 }
