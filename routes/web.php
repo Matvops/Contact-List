09315fc745d3dc815849b4caf63a13ff0584ee23
Matheus <matheusmartinscia@gmail.com>
2025-03-09 21:28:30 -0300

    Add middlewares for user logged and user not logged

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,23 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\SignInController;
+use App\Http\Middleware\UserIsLogged;
+use App\Http\Middleware\UserIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
 //NonLogged
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/authLogin', [AuthController::class, 'loginSubmit'])->name('authLogin');
-Route::get('/createAccount', [SignInController::class, 'SignIn'])->name('sign');
-Route::post('/createAccountSubmit', [SignInController::class, 'SignInSubmit'])->name('signSubmit');
+Route::middleware(UserIsNotLogged::class)->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/authLogin', [AuthController::class, 'loginSubmit'])->name('authLogin');
+    Route::get('/createAccount', [SignInController::class, 'SignIn'])->name('sign');
+    Route::post('/createAccountSubmit', [SignInController::class, 'SignInSubmit'])->name('signSubmit');
+});
 
 //Logged
-Route::get('/', [MainController::class, 'home'])->name('home');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/updateContact/{id}', [MainController::class, 'update'])->name('update');
-Route::post('/updateContactSubmit', [MainController::class, 'updateSubmit'])->name('updateSubmit');
-Route::delete('/deleteContact/{id}', [MainController::class, 'delete'])->name('delete');
+Route::middleware(UserIsLogged::class)->group(function () {
+    Route::get('/', [MainController::class, 'home'])->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/updateContact/{id}', [MainController::class, 'update'])->name('update');
+    Route::post('/updateContactSubmit', [MainController::class, 'updateSubmit'])->name('updateSubmit');
+    Route::delete('/deleteContact/{id}', [MainController::class, 'delete'])->name('delete');
+});
